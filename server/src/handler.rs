1d8a2170ef7056baebde4b88a9882a4208d3d14f
oselgithub <oselgihub@seznam.cz>
2016-01-26 11:00:55 +0100

    * add handler

--- server/src/handler.rs
+++ server/src/handler.rs
@@ -1,16 +1,24 @@
+extern crate rustc_serialize;
+
+use iron::error::IronError;
 use iron::middleware::Handler;
 use iron::prelude::{ Request, IronResult, Response };
 use iron::status;
-use std::sync::Arc;
+use rustc_serialize::json;
+use rustc_serialize::json::DecodeResult;
+use std::io::Read;
+use std::sync::{ Arc, Mutex };
 
-use model::author::AuthorProvider;
+use model::author::{ Author, AuthorProvider };
 
+/// Add author http handler
 pub struct AddHandler {
-  authors: Arc< Box< AuthorProvider > >,
+  /// author provider
+  authors: Arc< Mutex< Box< AuthorProvider > > >,
 }
 
 impl AddHandler {
-  pub fn new(authors: &Arc< Box< AuthorProvider > >) -> AddHandler {
+  pub fn new(authors: &Arc< Mutex< Box< AuthorProvider > > >) -> AddHandler {
     AddHandler {
       authors: authors.clone(),
     }
@@ -19,6 +27,15 @@ impl AddHandler {
 
 impl Handler for AddHandler {
   fn handle(&self, request: &mut Request) -> IronResult< Response > {
-    Ok(Response::with((status::Ok, "OK")))
+    let mut body = String::new();
+    request.body.read_to_string(&mut body).unwrap();
+    let author: DecodeResult< Author > = json::decode(&body);
+    match author {
+      Ok(author) => {
+        self.authors.lock().unwrap().add(&author);
+        Ok(Response::with((status::Ok, "OK")))
+      }
+      Err(error) => Err(IronError::new(error, "Invalid message body"))
+    }
   }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -6,7 +6,7 @@ extern crate rustc_serialize;
 use iron::prelude::{ Request, IronResult, Response, Iron };
 use iron::status;
 use router::Router;
-use std::sync::Arc;
+use std::sync::{ Arc, Mutex };
 
 mod handler;
 mod model;
@@ -16,19 +16,10 @@ use model::author::AuthorProvider;
 use model::memoryprovider::{ AuthorMemoryProvider };
 
 fn main() {
-  let mut authors: Arc< Box< AuthorProvider > > = Arc::new(Box::new(AuthorMemoryProvider::new()));
+  let authors: Arc< Mutex< Box< AuthorProvider > > > = Arc::new(Mutex::new(Box::new(AuthorMemoryProvider::new())));
   fn hello_world(_: &mut Request) -> IronResult< Response > {
     Ok(Response::with((status::Ok, "Hello World!")))
   }
-  // fn get_author(request: &mut Request) -> IronResult< Response > {
-  //   let authors = Rc::get(authors);
-  //   let body = request.body();
-  //   let predicate = |author: &&Author| author.get_first_name() == "Donald";
-  //   Ok(Response::with((status::Ok, "Hello World!")))
-  // }
-  // fn add_author(request: &mut Request) -> IronResult< Response > {
-  //   let mut authors = &authors;
-  // }
   let router = router!(
     get "/" => hello_world,
     post "/add" => AddHandler::new(&authors));
