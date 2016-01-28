f788f47918af9f5b86430f4981b08d8a3611aa9f
oselgithub <oselgihub@seznam.cz>
2016-01-28 10:31:57 +0100

    client demo

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,3 +1,13 @@
+extern crate hyper;
+
+use hyper::Client;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    let client = Client::new();
+    let mut res = client.get("http://localhost:3000/author/get_all").send().unwrap();
+    assert_eq!(res.status, hyper::Ok);
+    let mut s = String::new();
+    res.read_to_string(&mut s).unwrap();
+    println!("{}", s);
 }
